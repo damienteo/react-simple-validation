66fadc2e2814f5482915354c57645e334601f2b3
damienteo <ksteo89@gmail.com>
2019-07-13 11:16:41 +0800

    Add state and logic

--- src/form/form.jsx
+++ src/form/form.jsx
@@ -2,17 +2,40 @@ import React, { Component } from "react";
 // import "./form.css";
 
 class Form extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleUserInput = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
+    const { name, email } = this.state;
     return (
       <form className="demoForm">
         <h2>Sign up</h2>
         <div className="form-group">
           <label htmlFor="email">Email address</label>
-          <input type="email" className="form-control" name="email" />
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            value={email}
+            onChange={event => this.handleUserInput(event)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" name="password" />
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            value={name}
+            onChange={event => this.handleUserInput(event)}
+          />
         </div>
         <button type="submit" className="btn btn-primary">
           Sign up
